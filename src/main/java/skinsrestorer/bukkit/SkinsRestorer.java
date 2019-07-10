4ed9def9edce0258dffd644c667325df18e98139
xknat <kevin.natte@gmail.com>
2019-07-10 12:56:26 +0200

    update fork

--- src/main/java/skinsrestorer/bukkit/SkinsRestorer.java
+++ src/main/java/skinsrestorer/bukkit/SkinsRestorer.java
@@ -19,6 +19,8 @@ import skinsrestorer.bukkit.skinfactory.UniversalSkinFactory;
 import skinsrestorer.shared.storage.Config;
 import skinsrestorer.shared.storage.Locale;
 import skinsrestorer.shared.storage.SkinStorage;
+import skinsrestorer.shared.update.UpdateChecker;
+import skinsrestorer.shared.update.UpdateCheckerGitHub;
 import skinsrestorer.shared.utils.*;
 
 import java.io.*;
@@ -35,7 +37,7 @@ public class SkinsRestorer extends JavaPlugin {
     private String configPath = "plugins" + File.separator + "SkinsRestorer" + File.separator + "";
 
     private boolean bungeeEnabled;
-    private UpdateDownloader updateDownloader;
+    private UpdateDownloaderGithub updateDownloader;
     private CommandSender console;
 
     public String getVersion() {
@@ -62,19 +64,35 @@ public class SkinsRestorer extends JavaPlugin {
             return;
         }
 
+        // Detect MundoSK
+        if (getServer().getPluginManager().getPlugin("MundoSK") != null) {
+            try {
+                YamlConfig mundoConfig = new YamlConfig("plugins" + File.separator + "MundoSK" + File.separator, "config", false);
+                mundoConfig.reload();
+                if (mundoConfig.getBoolean("enable_custom_skin_and_tablist")) {
+                    console.sendMessage("§e[§2SkinsRestorer§e] §a----------------------------------------------");
+                    console.sendMessage("§e[§2SkinsRestorer§e] §cWe have detected MundoSK on your server with §e'enable_custom_skin_and_tablist: true'§c.");
+                    console.sendMessage("§e[§2SkinsRestorer§e] §cThat setting is located in §e/plugins/MundoSK/config.yml");
+                    console.sendMessage("§e[§2SkinsRestorer§e] §cYou have to disable it to get SkinsRestorer to work.");
+                    console.sendMessage("§e[§2SkinsRestorer§e] §a----------------------------------------------");
+                }
+            } catch (Exception ignored) {
+            }
+        }
+
         // Check if we are running in bungee mode
         this.checkBungeeMode();
 
         // Check for updates
         if (Config.UPDATER_ENABLED) {
-            this.updateChecker = new UpdateChecker(2124, this.getDescription().getVersion(), this.getLogger(), "SkinsRestorerUpdater/Bukkit");
-            this.updateDownloader = new UpdateDownloader(this);
+            this.updateChecker = new UpdateCheckerGitHub(2124, this.getDescription().getVersion(), this.getLogger(), "SkinsRestorerUpdater/Bukkit");
+            this.updateDownloader = new UpdateDownloaderGithub(this);
             this.checkUpdate(bungeeEnabled);
 
             if (Config.UPDATER_PERIODIC)
                 this.getServer().getScheduler().runTaskTimerAsynchronously(this, () -> {
                     this.checkUpdate(bungeeEnabled, false);
-                }, 20 * 60 * 30, 20 * 60 * 30);
+                }, 20 * 60 * 5, 20 * 60 * 5);
         }
 
         if (bungeeEnabled) {
